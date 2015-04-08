8143ea2ecd8f513217292c084a0334d13e689e78
free1 <747549945@qq.com>
2015-04-08 15:17:42 +0800

    改进spider

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
-source 'https://rubygems.org'
+# source 'https://rubygems.org'
+source "http://ruby.taobao.org"
 
 # 解析页面
 gem 'nokogiri'
+# 解决ruby部分版本open-uri的bug
+gem 'open_uri_redirections'
 # 队列
 # gem 'sidekiq'
--- lib/free_spider/begin.rb
+++ lib/free_spider/begin.rb
@@ -1,10 +1,18 @@
+# encoding = utf-8
+# 生成gem
+# gem build free_spider.gemspec
+# 安装gem
+# gem install free_spider
 # 程序入口
 # require 'free_spider'
 # spider = FreeSpider::Begin.new
 # spider.plan do
-#   site 'http://ruby-china.org'
+#   site 'http://www.dfrobot.com.cn/'
 # end
 # spider.crawl
+#调试
+# pry -Ilib -rfree_spider
+# irb -Ilib -rfree_spider
 
 require 'open-uri'
 require 'nokogiri'
@@ -18,6 +26,7 @@ module FreeSpider
       @todo = []
       # 已经访问过的链接
       @visited = []
+      @titles = []
     end
 
     # 程序制定函数，用户选择需要抓取的网页内容
@@ -31,20 +40,45 @@ module FreeSpider
 
     # 查找网页中的链接
     def find_link(path)
-      crawl if path == nil
-      html = open(path).read
-      # 访问过的链接放入数组
-      @visited << path
-      doc = Nokogiri::HTML(html)
-      doc.css("a").map do |href|
-        href = href.attributes["href"].value
-        href = @site + href unless href.include?("#{@site}")
-        @todo << href
+      p "find_link-------------------"
+      begin
+        crawl if path == nil
+        html = open(path).read
+        # html = open('http://www.dfrobot.com.cn/').read
+        # 访问过的链接放入数组
+        @visited << path
+        # p "================"
+        # p @visited
+        # p path
+        doc = Nokogiri::HTML(html)
+        # 抓取主要内容
+        doc.css("a").map do |href|
+          # 选取内容
+          title = href.attributes["title"]
+          title_content = href.attributes["title"].value unless title.nil? && @titles.include?(title)
+          # 处理链接
+          href = href.attributes["href"].value unless href.attributes["href"].nil?
+          href = @site + href unless href.include?("#{@site}")
+          @todo << href
+          @titles << title_content
+        end
+        # 去除重复链接
+        @todo.uniq
+        puts "#{@visited}"
+        p @titles.compact
+        crawl
+      rescue OpenURI::HTTPError
+        puts "404"
+        crawl
+      rescue RuntimeError
+        puts "redirection forbidden"
+        crawl
+      rescue URI::InvalidURIError
+        puts "bad URI"
+        crawl
+      ensure
+
       end
-      # 去除重复链接
-      @todo.uniq
-      puts "#{@visited}"
-      crawl
     end
 
     # 程序开始函数
@@ -61,6 +95,8 @@ module FreeSpider
       end
       if path.nil?
         puts "结束"
+        # 输出抓取内容
+        # post_title
         return
       end
       find_link(path)
@@ -68,12 +104,17 @@ module FreeSpider
 
     # 需要爬取的网站首页
     def site(url)
+      p "-----------------"
       if url.empty?
         puts "URL is blank"
       else
         @site = url
         @todo << @site
       end
+    end
+
+    def post_title
+      @titles.uniq.compact
     end
 
   end
