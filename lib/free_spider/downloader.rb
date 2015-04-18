78f59d3d4c045b71f498114a05b9fc0817fc346e
free1 <747549945@qq.com>
2015-04-18 15:33:03 +0800

    test

--- lib/free_spider/downloader.rb
+++ lib/free_spider/downloader.rb
@@ -10,29 +10,27 @@ module FreeSpider
     # 数据库配置文件
     # spec = Gem::Specification.find_by_name("database")
     # gem_root = spec.gem_dir
-  #   p "============"
-  #   p $LOAD_PATH
-  #   environment = ENV['RACK_ENV'] || 'development'
-  #   dbconfig = YAML.load(File.read("#{$LOAD_PATH.first}/config/database.yml"))
+    environment = ENV['RACK_ENV'] || 'development'
+    dbconfig = YAML.load(File.read("#{$LOAD_PATH.first}/config/database.yml"))
 
-		# # 链接数据库
-		# puts "----database_connection-----"
-  # 	ActiveRecord::Base.establish_connection(dbconfig[environment])
+		# 链接数据库
+		puts "----database_connection-----"
+  	ActiveRecord::Base.establish_connection(dbconfig[environment])
 
-  # 	# 创建表结构
-  # 	puts "----table_create-----"
-  #   ActiveRecord::Schema.define do
-		#   unless ActiveRecord::Base.connection.tables.include? 'news_teachings'
-		#     create_table :news_teachings do |table|
-		#       table.column :title,     :string
-		#       table.column :content,   :text
-		#     end
-		#   end
-		# end
+  	# 创建表结构
+  	puts "----table_create-----"
+    ActiveRecord::Schema.define do
+		  unless ActiveRecord::Base.connection.tables.include? 'news_teachings'
+		    create_table :news_teachings do |table|
+		      table.column :title,     :string
+		      table.column :content,   :text
+		    end
+		  end
+		end
 
-  # 	class NewsTeaching < ActiveRecord::Base
-  # 		validates_presence_of :title, :content
-  # 	end
+  	class NewsTeaching < ActiveRecord::Base
+  		validates_presence_of :title, :content
+  	end
 
   end
 end
