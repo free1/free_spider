3ec52b411f8b5ae7e5f665ebaeec77cd6dea9095
free <free@makehave.com>
2017-04-12 10:51:37 +0800

    fix

--- lib/free_spider/downloader.rb
+++ lib/free_spider/downloader.rb
@@ -5,7 +5,7 @@ require 'mysql2'
 
 module FreeSpider
   module Downloader
-  	ActiveRecord::Base.logger = Logger.new(STDERR)
+    ActiveRecord::Base.logger = Logger.new(STDERR)
 
     # 数据库配置文件
     # spec = Gem::Specification.find_by_name("database")
@@ -13,9 +13,9 @@ module FreeSpider
     # environment = ENV['RACK_ENV'] || 'development'
     # dbconfig = YAML.load(File.read("#{$LOAD_PATH.first}/config/database.yml"))
 
-		# 链接数据库
-		puts "----database_connection-----"
-  	# ActiveRecord::Base.establish_connection(dbconfig[environment])
+    # 链接数据库
+    puts "----database_connection-----"
+    # ActiveRecord::Base.establish_connection(dbconfig[environment])
     ActiveRecord::Base.establish_connection(
       adapter: 'mysql2',
       host: 'localhost',
@@ -24,20 +24,20 @@ module FreeSpider
       password: '123'
     )
 
-  	# 创建表结构
-  	puts "----table_create-----"
+    # 创建表结构
+    puts "----table_create-----"
     ActiveRecord::Schema.define do
-		  unless ActiveRecord::Base.connection.tables.include? 'news_teachings'
-		    create_table :news_teachings do |table|
-		      table.column :title,     :string
-		      table.column :content,   :text
-		    end
-		  end
-		end
+      unless ActiveRecord::Base.connection.tables.include? 'news_teachings'
+        create_table :news_teachings do |table|
+          table.column :title,     :string
+          table.column :content,   :text
+        end
+      end
+    end
 
-  	class NewsTeaching < ActiveRecord::Base
-  		validates_presence_of :title, :content
-  	end
+    class NewsTeaching < ActiveRecord::Base
+      validates_presence_of :title, :content
+    end
 
   end
 end
